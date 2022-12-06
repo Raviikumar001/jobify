f0cd6cec78e6735993015d4ad64df144b96ac3b1
ravi <samcollins9899@gmail.com>
2022-12-06 06:00:16 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+        <h2>Jobify</h2>
+      </div>
+      )
+}
+
+export default App
