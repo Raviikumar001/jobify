5f18065277bbd8acd3ff0de9827a255a48eb5731
Ravi <samcollins9899@gmail.com>
2022-12-13 10:42:59 +0530

    added dotenv

---
+++ server/middleware/not-Found.js
@@ -0,0 +1,8 @@
+
+const  notFoundMiddleware = (req,res)=>{
+
+    return res.status(404).send('Route does not exist');
+
+}
+
+export default notFoundMiddleware;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,27 @@
+import express from "express";
 
-import express from 'express';
+//middlewares
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+import notFoundMiddleware from "./middleware/not-Found.js";
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+
 
 const app = express();
 
-app.get('/',(req, res)=> {
-
-    res.send('Welcome');
-})
+//notFoundMiddleware
 
 
-const port = process.env. PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
-app.listen(port, ()=>{
-    console.log(`server is listening on port :${port}`, )
-})
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`server is listening on port :${port}`);
+});
