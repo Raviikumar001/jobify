b8b5075008e3c3539f58c3498c05bafa7f92c0fc
ravi <samcollins9899@gmail.com>
2022-12-13 09:00:19 +0530

    added server

---
+++ server/server.js
@@ -0,0 +1,16 @@
+
+import express from 'express';
+
+const app = express();
+
+app.get('/',(req, res)=> {
+
+    res.send('Welcome');
+})
+
+
+const port = process.env. PORT || 5000;
+
+app.listen(port, ()=>{
+    console.log(`server is listening on port :${port}`, )
+})
