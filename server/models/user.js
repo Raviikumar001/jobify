f271f156053b7493df1c4541b9fb5790783c0975
Ravi <samcollins9899@gmail.com>
2022-12-16 22:50:26 +0530

    resolve error1

--- server/models/user.js
+++ server/models/user.js
@@ -14,7 +14,7 @@ const UserSchema = new mongoose.Schema({
     required: [true, "Please provide email"],
     validate: {
         validator: validator.isEmail,
-        messatge: 'Please provide and email'
+        message: 'Please provide a valid email'
     },
     unique: true
   },
