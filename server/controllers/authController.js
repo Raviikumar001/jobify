1db65e87cbcc9868aec1cb8e6837bf80c8161423
Ravi <samcollins9899@gmail.com>
2022-12-17 19:59:07 +0530

    refactor 1

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,10 +1,39 @@
 import User  from "../models/user.js";
 import { StatusCodes } from "http-status-codes";
 
-const register = async (req,res, next)=>{
+class CustomApiError extends Error{
 
-        const user = await User.create(req.body);
-        res.status(StatusCodes.CREATED).json({user});
+    constructor(message)
+    {
+        super(message)
+    }
+}
+ 
+class BadRequestError extends CustomApiError {
+    constructor(message){
+        super(message)
+        this.statusCode = StatusCodes.BAD_REQUEST;
+
+    }
+
+}
+
+class NotFoundError extends CustomApiError {
+    constructor(message){
+        super(message)
+        this.statusCode = StatusCodes.NOT_FOUND
+    }
+}
+
+
+const register = async (req,res, next)=>{
+        const {name, email, password} = req.body;
+        if(!name  || !email || !password )
+        {
+            throw new BadRequestError('Please provide all values');
+        }
+        const user = await User.create({name, email,password});
+        res.status(StatusCodes.CREATED).json({user}); 
 
       
 
